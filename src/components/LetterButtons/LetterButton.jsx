6832a3e0c3605c1e8a46f3d166907b81a3c6af65
vaibhav484848 <113878147+vaibhav484848@users.noreply.github.com>
2025-04-01 20:14:03 +0530

    Added Hangman

---
+++ src/components/LetterButtons/LetterButton.jsx
@@ -0,0 +1,51 @@
+const ALPHABETS="QWERTYUIOPASDFGHJKLZXCVBNM";
+
+function LetterButtons({text,guessedLetters,onLetterClick,updateStep}){
+    
+    const originalLetters= new Set(text.toUpperCase().split(''));
+    const guessedLetterSet= new Set(guessedLetters);
+
+    // function onClickHandler(e){
+    //     onLetterClick(e.target.innerText);
+    // }
+
+    const buttonStyle=function(letter){
+        if(guessedLetterSet.has(letter)){
+            if(originalLetters.has(letter)){
+                return 'bg-green-500'
+            }
+            else{
+                
+                return 'bg-red-500'
+            }
+            // return `${originalLetters.has(letter)?'bg-green-500':'bg-red-500'}`;
+        }
+        else{
+            return 'bg-blue-500';
+        }
+    }
+
+    const buttons=ALPHABETS.split('').map(letter=>{
+        return (
+            <button
+                key={`button -${letter}`}
+                onClick={onLetterClick}
+                disabled={guessedLetterSet.has(letter)}
+                // className=""
+                className={`h-12 w-12 m-1 rounded-md focus:outline-none text-white ${buttonStyle(letter)}`}
+                >
+                    {letter}
+                </button>
+        )
+    });
+
+    return (
+        <>
+         {buttons}
+        </>
+       
+    )
+
+}
+
+export default LetterButtons;
--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -1,13 +1,38 @@
 import { useLocation } from "react-router-dom";
 import MasKedText from "../components/MaskedText/MaskedText";
+import LetterButtons from "../components/LetterButtons/LetterButton";
+import { useState } from "react";
+import HangMan from "../components/HangMan/HangMan";
 
 function PlayGame(){
     const{state} =useLocation();
+
+    const [guessedLetters,setGuessedLetter]=useState([]);
+
+    const [step,setStep]=useState(0);
+
+    function stepUpdate(){
+        setStep(step+1);
+    }
+
+    function onLetterClick(event){
+        
+        if(!state.wordSelected.toUpperCase().includes(event.target.innerText)) setStep(step+1);
+        const newArr=[...guessedLetters,event.target.innerText];
+        setGuessedLetter(newArr);
+
+
+
+    }
+
     return(
         <>
             <h1>PlayGame {state.wordSelected}</h1>
 
-            <MasKedText text={state.wordSelected} guessedLetters={['h','e']}/>
+            <MasKedText text={state.wordSelected} guessedLetters={guessedLetters} />
+            <LetterButtons text={state.wordSelected} guessedLetters={guessedLetters} onLetterClick={onLetterClick} />
+
+            <HangMan step={step}/>
         </>
     )
 }
